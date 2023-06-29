cdfa16d734624d5fea8580b29bf8678d7b0485f1
Adriana Benedetti <adrianabenedetti@192.168.1.6>
2023-06-29 23:06:08 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const PORT = 5050;
+
+const server = express();
+
+mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Errore di connessione al DB'));
+db.once('open', () => {
+    console.log('DB connesso correttamente')
+});
+
+server.listen(PORT, () => console.log(`Server avviato sulla porta ${PORT}`))
